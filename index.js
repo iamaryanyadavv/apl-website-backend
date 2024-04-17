b5c7fd2276053f4760b0a92dc5d86e4f6a894e1f
zahaanshapoorjee <zahaan@shapoorjee.com>
2024-04-17 21:44:29 +0530

    lol

--- index.js
+++ index.js
@@ -219,12 +219,13 @@ app.get('/fantasy/apl7/playerdata', async (req,res)=>{
 
 app.post('/fantasy/submit', async (req, res) => {
     try {
-        const client = await auth.getClient();
-        const googleSheets = google.sheets({ version: 'v4', auth: client });
+   
         const auth = new google.auth.GoogleAuth({
             keyFile: 'credentials.json',
             scopes: 'https://www.googleapis.com/auth/spreadsheets'
         })
+        const client = await auth.getClient();
+        const googleSheets = google.sheets({ version: 'v4', auth: client });
         const response = await googleSheets.spreadsheets.values.append({
             spreadsheetId: APL7spreadsheetID,
             range: "FantasySubmissions",
